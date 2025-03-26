f54162cc5f1856e8832077ebd31230fe60d96863
Nikusha Kvatsabaia <46217588+Kv4ts0@users.noreply.github.com>
2025-03-26 13:58:23 +0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -90,3 +90,18 @@ function showInfo(index) {
   document.getElementById("infoDesc").innerText = locations[index].description;
   document.getElementById("infoImage").src = locations[index].image;
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
